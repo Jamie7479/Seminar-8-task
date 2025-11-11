8aa927b7f2408f9b23ec2b3d52df4de0b937d520
Jamie White <jamie.white1@my.jcu.edu.au>
2025-11-11 10:21:30 +1000

    a

--- shopping_app.py
+++ shopping_app.py
@@ -1,16 +1,21 @@
-
-
 from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.button import  Button
+from kivy.uix.button import Button
+from product import Product
+
 
 class ShoppingApp(App):
+    price_text = StringProperty()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.products = [{"name": "Cheese", "price": 12.5}, {"name": "Laptop", "price": 912.95}, {"name": "Plant", "price": 4.75},
-         {"name": "Coffee Machine", "price": 2300.00}, {"name": "Guitar", "price": 4399.95}]
+        self.products = [Product(product["name"], product["price"]) for product in
+                         [{"name": "Cheese", "price": 12.5}, {"name": "Laptop", "price": 912.95},
+                          {"name": "Plant", "price": 4.75},
+                          {"name": "Coffee Machine", "price": 2300.00}, {"name": "Guitar", "price": 4399.95}]]
+        self.total_price = 0.0
 
     def build(self):
         """Build the Kivy GUI."""
@@ -21,14 +26,15 @@ class ShoppingApp(App):
         return self.root
 
     def create_widgets(self):
+        self.price_text = f"Total cost: ${self.total_price:,.2f}"
         for product in self.products:
             temp_button = Button(text=str(product))
             temp_button.bind(on_release=self.press_entry)
             temp_button.product = product
             self.root.ids.entries_box.add_widget(temp_button)
 
-    def press_entry(self, instance):
-        pass
+    def press_entry(self, button):
+        self.total_price += button.product.price
 
 
 ShoppingApp().run()
